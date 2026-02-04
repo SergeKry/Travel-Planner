24bdd714e2b48253438e1eab5f278ffccaafb0ac
Serhii Kryvosheiev <krivosheevsrg@gmail.com>
2026-02-04 14:48:42 +0100

    [FEAT] - add basic project setup and healthcheck endpoint

---
+++ api/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(["GET"])
+def health(request):
+    return Response({"status": "ok"})
